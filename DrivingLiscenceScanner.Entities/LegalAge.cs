7f5b27215ed5b17b895b33e997d8580e1a57e8e4
AbdulMuqsit <abdulmuqsitbutt@live.com>
2015-01-10 16:09:20 +0500

    Window Changed

---
+++ DrivingLiscenceScanner.Entities/LegalAge.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrivingLicenceScanner.Entities.Infrastructure;
+
+namespace DrivingLicenceScanner.Entities
+{
+    class LegalAge:ObjectBase
+    {
+        private int _cigerates;
+        private int _liquor;
+        private int _lottery;
+
+        public int Cigarette
+        {
+            get { return _cigerates; }
+            set
+            {
+                if (value == _cigerates) return;
+                _cigerates = value;
+                OnPropertyChanged("Cigerates");
+            }
+        }
+
+        public int Liquor
+        {
+            get { return _liquor; }
+            set
+            {
+                if (value == _liquor) return;
+                _liquor = value;
+                OnPropertyChanged("Liquor");
+            }
+        }
+
+        public int Lottery
+        {
+            get { return _lottery; }
+            set
+            {
+                if (value == _lottery) return;
+                _lottery = value;
+                OnPropertyChanged("Lottery");
+            }
+        }
+    }
+}
--- DrivingLiscenceScanner/MainWindow.xaml.cs
+++ DrivingLiscenceScanner/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using Amuqsit.CustomControls.Windows;
 
 namespace DrivingLicenceScanner
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : CustomWindow
     {
         public MainWindow()
         {
