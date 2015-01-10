b949a6b109e1e135799a6658fd8bce9cf175b961
AbdulMuqsit <abdulmuqsitbutt@live.com>
2015-01-10 19:15:57 +0500

    Changed age limit class, Add DbContext

--- DrivingLiscenceScanner.Entities/LegalAge.cs
+++ DrivingLiscenceScanner.Entities/LegalAge.cs
@@ -7,43 +7,10 @@ using DrivingLicenceScanner.Entities.Infrastructure;
 
 namespace DrivingLicenceScanner.Entities
 {
-    class LegalAge:ObjectBase
+    public class LegalAge : ObjectBase
     {
-        private int _cigerates;
-        private int _liquor;
-        private int _lottery;
-
-        public int Cigarette
-        {
-            get { return _cigerates; }
-            set
-            {
-                if (value == _cigerates) return;
-                _cigerates = value;
-                OnPropertyChanged("Cigerates");
-            }
-        }
-
-        public int Liquor
-        {
-            get { return _liquor; }
-            set
-            {
-                if (value == _liquor) return;
-                _liquor = value;
-                OnPropertyChanged("Liquor");
-            }
-        }
-
-        public int Lottery
-        {
-            get { return _lottery; }
-            set
-            {
-                if (value == _lottery) return;
-                _lottery = value;
-                OnPropertyChanged("Lottery");
-            }
-        }
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Age { get; set; }
     }
 }
---
+++ DrivingLiscenceScanner.EntityFramework/DrivingLicenceScannerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrivingLicenceScanner.Entities;
+
+namespace DrivingLicenceScanner.EntityFramework
+{
+    class DrivingLicenceScannerDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CheckIn> CheckIns { get; set; }
+        public DbSet<LegalAge> LegalAges { get; set; }
+        public DbSet<Licence> Licences { get; set; }
+    }
+}
