2164c338c9a10639d9da8c1c1ad7f6ae629b9195
Abdul Muqsit <abdemuqsit@hotmail.com>
2015-02-10 23:07:53 +0500

    Delivered

--- DrivingLiscenceScanner/MainWindow.xaml.cs
+++ DrivingLiscenceScanner/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Amuqsit.CustomControls.Windows;
+﻿using System.Diagnostics;
+using System.Windows.Navigation;
+using Amuqsit.CustomControls.Windows;
 
 namespace DrivingLicenceScanner
 {
@@ -10,6 +12,11 @@ namespace DrivingLicenceScanner
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
