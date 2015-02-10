a140a6853847f853815c276552e99441c14ac1b2
Abdul Muqsit <abdemuqsit@hotmail.com>
2015-02-10 19:05:16 +0500

    Setup

---
+++ DrivingLiscenceScanner/Model/CustomerLegalStatus.cs
@@ -0,0 +1,32 @@
+﻿using DrivingLicenceScanner.Entities.Infrastructure;
+
+namespace DrivingLicenceScanner.Model
+{
+    public class CustomerLegalStatus : ObjectBase
+    {
+        private bool _allowed;
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == _name) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool Allowed
+        {
+            get { return _allowed; }
+            set
+            {
+                if (value.Equals(_allowed)) return;
+                _allowed = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
